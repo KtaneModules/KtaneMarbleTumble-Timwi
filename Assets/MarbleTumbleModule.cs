a57886225e7ff87b9d4587e2610485ea44a37268
Timwi <github@timwi.de>
2018-05-03 15:17:23 +0200

    Rename to “MarbleTumble” and make the traps in all the cylinders equiangular (currently 36°)

--- Assets/MarbleTumbleModule.cs
+++ Assets/MarbleTumbleModule.cs
@@ -10,7 +10,7 @@ using Rnd = UnityEngine.Random;
 /// On the Subject of Tumble Lock
 /// Created by Timwi
 /// </summary>
-public class TumbleLockModule : MonoBehaviour
+public class MarbleTumbleModule : MonoBehaviour
 {
     public KMBombInfo Bomb;
     public KMBombModule Module;
@@ -20,20 +20,24 @@ public class TumbleLockModule : MonoBehaviour
     public Mesh[] Meshes;
     public Texture[] Textures;
     public KMSelectable Selectable;
-    public GameObject Marble;
-    public GameObject MarbleContainer;
+    public GameObject MarbleLayer1; // layer used for rotation to match the rotation of the cylinder it’s in
+    public GameObject MarbleLayer2; // layer used to “roll” the marble
+    public GameObject MarbleLayer3; // layer used to rotate the marble about its center randomly
 
     private static int _moduleIdCounter = 1;
     private int _moduleId;
 
     // Red, yellow, green, blue, silver
     private static Color[] _colors = "FF8181,EFF09A,81D682,8EB5FF,FFFFFF".Split(',').Select(str => new Color(Convert.ToInt32(str.Substring(0, 2), 16) / 255f, Convert.ToInt32(str.Substring(2, 2), 16) / 255f, Convert.ToInt32(str.Substring(4, 2), 16) / 255f)).ToArray();
-    private static int[] _numNotches = new[] { 4, 5, 6, 8, 10 };
+    private static int _numNotches = 10;
+    private static int[] _notchMins = new[] { 3, 2, 1, 1, 1 };
+    private static int[] _notchMaxs = new[] { 7, 8, 9, 9, 9 };
     private static int[][] _rotationData = @"-1,1,-2,0,2;-2,1,2,-1,0;1,0,2,-2,-1;0,-1,-2,1,2;2,0,1,-1,-2;1,-2,-1,2,0;-2,2,0,1,-1;0,-1,1,2,-2;-1,2,0,-2,1;2,-2,-1,0,1".Split(';').Select(str => str.Split(',').Select(s => int.Parse(s)).ToArray()).ToArray();
 
     private int[] _traps;
     private int[] _colorIxs;
     private int[] _rotations;
+    private int _marbleDist;
 
     private sealed class RotationInfo
     {
@@ -41,7 +45,49 @@ public class TumbleLockModule : MonoBehaviour
         public int RotateFrom;
         public int RotateTo;
     }
-    private Queue<RotationInfo[]> _queue = new Queue<RotationInfo[]>();
+    private abstract class Anim
+    {
+        public abstract IEnumerable<object> RunAnimation(MarbleTumbleModule m);
+    }
+    private sealed class RotationAnim : Anim
+    {
+        public RotationInfo[] Rotations;
+
+        public override IEnumerable<object> RunAnimation(MarbleTumbleModule m)
+        {
+            var elapsed = 0f;
+            var duration = .1f;
+            var fromAngles = Rotations.Select(inf => (360 / _numNotches) * inf.RotateFrom).ToArray();
+            var toAngles = Rotations.Select(inf => (360 / _numNotches) * inf.RotateTo).ToArray();
+            while (elapsed < duration)
+            {
+                elapsed += Time.deltaTime;
+                for (int i = 0; i < 5; i++)
+                    m.Cylinders[i].transform.localEulerAngles = new Vector3(0, easeOutSine(Mathf.Min(duration, elapsed), duration, fromAngles[i], toAngles[i]), 0);
+                yield return null;
+            }
+        }
+    }
+    private sealed class MarbleIntoGap : Anim
+    {
+        public int IntoIndex;
+
+        public override IEnumerable<object> RunAnimation(MarbleTumbleModule m)
+        {
+            yield break;
+        }
+    }
+    private sealed class MarbleIntoTrap : Anim
+    {
+        public int IntoIndex;
+
+        public override IEnumerable<object> RunAnimation(MarbleTumbleModule m)
+        {
+            yield break;
+        }
+    }
+
+    private Queue<Anim> _queue = new Queue<Anim>();
     private Coroutine _rotateCoroutine;
 
     void Start()
@@ -50,16 +96,17 @@ public class TumbleLockModule : MonoBehaviour
         _colorIxs = Enumerable.Range(0, 5).ToArray().Shuffle();
         _rotations = new int[5];
         _traps = new int[5];
+        _marbleDist = 6;
 
         enqueueRotations(() =>
         {
             var textureIxs = Enumerable.Range(0, 5).ToArray().Shuffle();
             for (int i = 0; i < 5; i++)
             {
-                _rotations[i] = Rnd.Range(i == 4 ? 1 : 0, _numNotches[i]);
+                _rotations[i] = Rnd.Range(i == 4 ? 1 : 0, _numNotches);
                 do
-                    _traps[i] = Rnd.Range(1, _numNotches[i]);
-                while (i == 4 && ((_rotations[i] + _traps[i]) % _numNotches[i] + _numNotches[i]) % _numNotches[i] == 0);
+                    _traps[i] = Rnd.Range(_notchMins[i], _notchMaxs[i] + 1);
+                while (i == 4 && trap(4) == 0);
 
                 var lookingFor = string.Format("Cylinder_{0}_{1}", i + 1, _traps[i]);
                 Cylinders[i].GetComponent<MeshFilter>().mesh = Meshes.First(m => m.name.Equals(lookingFor));
@@ -68,7 +115,7 @@ public class TumbleLockModule : MonoBehaviour
                 mat.mainTexture = Textures[textureIxs[i]];
             }
         });
-        Marble.transform.localEulerAngles = new Vector3(Rnd.Range(0, 360), Rnd.Range(0, 360), Rnd.Range(0, 360));
+        MarbleLayer3.transform.localEulerAngles = new Vector3(Rnd.Range(0, 360), Rnd.Range(0, 360), Rnd.Range(0, 360));
 
         _rotateCoroutine = StartCoroutine(rotate());
         Bomb.OnBombExploded += delegate { StopCoroutine(_rotateCoroutine); };
@@ -81,10 +128,28 @@ public class TumbleLockModule : MonoBehaviour
         action();
         for (int i = 0; i < 5; i++)
             infs[i].RotateTo = _rotations[i];
-        _queue.Enqueue(infs);
+        _queue.Enqueue(new RotationAnim { Rotations = infs });
+        if (_marbleDist > 0)
+        {
+            var marblePos = _marbleDist == 6 ? 0 : gap(_marbleDist);
+            if (gap(_marbleDist - 1) == marblePos)
+                _queue.Enqueue(new MarbleIntoGap { IntoIndex = _marbleDist - 1 });
+            else if (trap(_marbleDist - 1) == marblePos)
+                _queue.Enqueue(new MarbleIntoTrap { IntoIndex = _marbleDist - 1 });
+        }
     }
 
-    private float easeOutSine(float time, float duration, float from, float to)
+    private int gap(int ix)
+    {
+        return (_rotations[ix] % _numNotches + _numNotches) % _numNotches;
+    }
+
+    private int trap(int ix)
+    {
+        return ((_rotations[ix] + _traps[ix]) % _numNotches + _numNotches) % _numNotches;
+    }
+
+    private static float easeOutSine(float time, float duration, float from, float to)
     {
         return (to - from) * Mathf.Sin(time / duration * (Mathf.PI / 2)) + from;
     }
@@ -97,17 +162,8 @@ public class TumbleLockModule : MonoBehaviour
                 yield return null;
 
             var infs = _queue.Dequeue();
-            var elapsed = 0f;
-            var duration = .1f;
-            var fromAngles = infs.Select(inf => (360 / _numNotches[inf.CylinderIndex]) * inf.RotateFrom).ToArray();
-            var toAngles = infs.Select(inf => (360 / _numNotches[inf.CylinderIndex]) * inf.RotateTo).ToArray();
-            while (elapsed < duration)
-            {
-                elapsed += Time.deltaTime;
-                for (int i = 0; i < 5; i++)
-                    Cylinders[i].transform.localEulerAngles = new Vector3(0, easeOutSine(Mathf.Min(duration, elapsed), duration, fromAngles[i], toAngles[i]), 0);
-                yield return null;
-            }
+            foreach (var obj in infs.RunAnimation(this))
+                yield return obj;
         }
     }
 
