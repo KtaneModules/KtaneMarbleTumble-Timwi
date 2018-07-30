5f28ff5c27646229bad5e54c60facc31e0e12088
Timwi <github@timwi.de>
2018-07-30 15:51:09 +0200

    Switch to CaitSith2’s colorblind mode component.

--- Assets/KMScripts/KMBombInfoExtensions.cs
+++ Assets/KMScripts/KMBombInfoExtensions.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Newtonsoft.Json;
 
-namespace MarbleTumble
+namespace KModkit
 {
     public enum Battery
     {
@@ -44,7 +44,8 @@ namespace MarbleTumble
         MSA,
         TRN,
         BOB,
-        FRK
+        FRK,
+        NLL
     }
 
     public enum IndicatorColor
